2623ca742f93feeb142349654006e2c7021a8994
Daniel Janeiro <dag.janeiro@gmail.com>
2015-11-24 19:02:21 +0000

    started using ES6 Maps

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ require('app-module-path').addPath(__dirname);
 const gen = require('lib/definition/generator');
 const fromDefinition = require('lib/runtime/model').fromDefinition;
 
+// Create a new workflow
 const workflow = gen.createWorkflow('My Workflow');
+
+// Create a new process.
 const process = gen.createProcess('My Process');
-const service = gen.createSystemService('My Service', function () {
-  console.log('hi!');
+
+// Define the process context variables.
+process.setContext(['a', 'b']);
+
+
+const service = gen.createSystemService('My Service', function (context) {
+  console.log('hi! ' + context.get('a'));
 });
+
+process.addService(service);
+process.setStartService(service);
+process.setEndService(service);
 
 
 workflow.addProcess(process).addService(service);
